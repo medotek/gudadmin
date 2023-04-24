c858cbb44404931a8a5026756bbe042426fdb87d
Thomas Huynh <thuynh@axome.com>
2023-04-24 22:21:30 +0200

    [*] Fix mention de role

--- src/Helper/NotificationRole.js
+++ src/Helper/NotificationRole.js
@@ -2,7 +2,7 @@ export function logsNotificationRole(type) {
     let mention = null;
     switch (type) {
         case 'website':
-            mention = `<@${process.env.GUDA_LOG_NOTIFICATION_ROLE_DISCORD}>`
+            mention = `<@&${process.env.GUDA_LOG_NOTIFICATION_ROLE_DISCORD}>`
             break;
     }
 
